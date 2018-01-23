eb0c091f4ba322db2dd1efb5cd0c16a03abcdad9
grepwood <grepwood@sucs.swan.ac.uk>
2018-01-23 20:12:48 +0100

    added more fault detection, added result token generator for PLATFORMS

--- update-tibia.sh
+++ update-tibia.sh
@@ -21,15 +21,19 @@ function sanity_check {
 		echo "tar is not installed. Tibia cannot be patched."
 		exit -2
 	fi
+	return 0
 }
 
 function download_tibia {
+	local result=0
 	printf "Downloading client... "
 	if [ "${download_program}" = "curl" ]; then
 		curl -L ${cipsoft_tibia_download_url}/${tibia_tarball} > ${tibia_tarball} 2>/dev/null
+		result=$?
 	else
 		if [ "${download_program}" = "wget" ]; then
 			wget ${cipsoft_tibia_download_url}/${tibia_tarball} -O ${tibia_tarball}
+			result=$?
 		else
 			echo "not possible"
 			echo "Could not find download program. Aborting"
@@ -37,21 +41,43 @@ function download_tibia {
 		fi
 	fi
 	echo "done"
+	return $result
 }
 
 function patch_tibia {
+	local result=0
 	printf "Extracting... "
 	tar -xf ${tibia_tarball}
+	result=$?
+	if [ $result -ne 0 ]; then
+		echo "unpacking failed with code $result"
+		exit $result
+	fi
 	echo "done"
 	local tmpdir=$(tar -tf ${tibia_tarball} | head -n1)
+	if [ "$tmpdir" = "" ]; then
+		echo "Could not find root directory inside the tarball"
+		exit -8
+	fi
 	pushd ${tmpdir} >/dev/null
 		printf "Patching... "
 		cp -rfp * ..
+		result=$?
+		if [ $result -ne 0 ]; then
+			echo "failed with code ${result}"
+			exit ${result}
+		fi
 		echo "done"
 	popd >/dev/null
 	printf "Cleaning up... "
 	rm -rf ${tibia_tarball} ${tmpdir}
+	result=$?
+	if [ ${result} -ne 0 ]; then
+		echo "failed with code $result"
+		exit $result
+	fi
 	echo "done"
+	return $result
 }
 
 function introduce_yourself {
@@ -85,11 +111,49 @@ function getopts {
 	set_global_variables
 }
 
+function generate_result_token {
+	local result=$1
+	local system=$(lsb_release -is)
+	local kernel=$(uname -s)
+	local kernel_version=$(uname -r | sed 's/\-.*$//')
+	local wget_present=$(if [ $(command -v wget >/dev/null 2>/dev/null; echo $?) -eq 0 ]; then echo Y; else echo N; fi)
+	local curl_present=$(if [ $(command -v curl >/dev/null 2>/dev/null; echo $?) -eq 0 ]; then echo Y; else echo N; fi)
+	local tar_family=""
+	local tar_version=""
+	local return_code=0
+	if [ $(tar --version | head -n1 | grep ^tar\ \(GNU | wc -l) -eq 1 ]; then
+		tar_family=GNU
+		tar_version=$(tar --version | head -n1 | awk '{print $4}')
+	fi
+	if [ $(tar --version | head -n1 | grep ^bsdtar | wc -l) -eq 1 ]; then
+		tar_family=BSD
+		tar_version=$(tar --version | head -n1 | awk '{print $2}')
+	fi
+	if [ "$tar_family" = "" ]; then
+		tar_family=UNKNOWN
+		tar_version=UNKNOWN
+	fi
+	if [ ${result} -eq 0 ]; then
+		result=Y
+	else
+		result=N
+		return_code=1
+	fi
+	echo "Here is your result token:"
+	printf "${system}\t${kernel}\t${kernel_version}\t${tar_family}\t${tar_version}\t${curl_present}\t${wget_present}\t${result}\n"
+	return ${return_code}
+}
+
 function main {
+	local result=0
 	getopts $@
 	sanity_check
+	result=$(((${result} + $?)))
 	download_tibia
+	result=$(((${result} + $?)))
 	patch_tibia
+	result=$(((${result} + $?)))
+	generate_result_token ${result}
 	echo "Starting Tibia"
 	./start-tibia.sh &
 }
